22eea52f4dc9807f2a15ecb146f4694ad676247b
Daniel Alberto Morales Beita <Dani9milpz@gmail.com>
2018-03-09 10:31:46 -0600

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -20,7 +20,7 @@ public class TestUtil extends TestBase{
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 	//String currentDir = System.getProperty("user.dir");
-		 String current = new java.io.File( "." ).getCanonicalPath();
+	public static	 String current = new java.io.File( "." ).getCanonicalPath();
      //   System.out.println("Current dir:"+current);
  //String currentDir = System.getProperty("user.dir");
 	public static String TESTDATA_SHEET_PATH = current
@@ -64,11 +64,11 @@ public class TestUtil extends TestBase{
 	public static void takeScreenshotAtEndOfTest() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		//String currentDir = System.getProperty("user.dir");
-		 String current2 = new java.io.File( "." ).getCanonicalPath();
+		// String current2 = new java.io.File( "." ).getCanonicalPath();
         System.out.println("Current dir:"+current);
  String currentDir = System.getProperty("user.dir");
         System.out.println("Current dir using System:" +currentDir);
-		FileUtils.copyFile(scrFile, new File(current2 + "/screenshots/" + System.currentTimeMillis() + ".png"));
+		FileUtils.copyFile(scrFile, new File(current + "/screenshots/" + System.currentTimeMillis() + ".png"));
 		
 		}
 	
